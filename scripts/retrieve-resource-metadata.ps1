7ad174c80b0a90616115f7f47e1ed5a99d82264c
Chris Sheldon <chris@automit.co.uk>
2020-09-13 18:39:37 +0100

    Cleanup

--- scripts/retrieve-resource-metadata.ps1
+++ scripts/retrieve-resource-metadata.ps1
@@ -1,6 +1,6 @@
 <# Retrieve Specific MetaData Information - Instructions
 1. Login to the instance you want to retrieve metadata for
-2. Copy this and 'nested-value-retrieval.psm1' to a scripts folder.
+2. Copy this file 'nested-value-retrieval.psm1' to a scripts folder.
 3. Open a terminal (Powershell Core is required)
 4. Run Powershell Core (psm1.exe)
 5. Set the $key variable to the key of the metadata key you want to retrieve (If this is a nested value use /) - Example 'compute/securityProfile/secureBootEnabled", use a comma if you wish to retrieve more than 1 value

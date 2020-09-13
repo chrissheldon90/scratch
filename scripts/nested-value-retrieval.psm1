906925ad25d2f14f0576938a11573192ff0deed8
Chris Sheldon <chris@automit.co.uk>
2020-09-13 15:49:20 +0100

    Added support for retrieving multiple values at once

--- scripts/nested-value-retrieval.psm1
+++ scripts/nested-value-retrieval.psm1
@@ -3,25 +3,26 @@ function Retrieve-NestedValue()
     [CmdletBinding()]
     param (
         [Parameter()][object]$object,
-        [Parameter()][string]$key
+        [Parameter()][object]$keys
     )
-    $ErrorActionPreference = "Stop"
-    $keySplit = ($key).Split("/")
-    $objectConverted = $object | ConvertFrom-Json
-    $value = "`$objectConverted"
-    foreach($keyName in $keySplit) {
-        
-        $memberCheck = (Invoke-Expression $value) | Get-Member $keyName
-        if(!$memberCheck) {
-            Write-Error "There is no key: $keyName"
+    foreach($key in $keys)
+    {
+        $keySplit = ($key).Split("/")
+        $objectConverted = $object | ConvertFrom-Json
+        $value = "`$objectConverted"
+        foreach($keyName in $keySplit) {
+            
+            $memberCheck = (Invoke-Expression $value) | Get-Member $keyName
+            $value += ".$keyName"
+            $valueExpression = Invoke-Expression $value
+            if(!$memberCheck) {
+                Write-Error "There is no key: $keyName"
+            } elseif(!$valueExpression) {
+                Write-Error "There is no key or value for key: $keyName"
+            }
         }
-        $value += ".$keyName"
-        $valueExpression = Invoke-Expression $value
-        if(!$valueExpression) {
-            Write-Error "There is no key or value for key: $keyName"
-        }
+        Write-Host $keyName":" $valueExpression
     }
-    $valueExpression
 }
 
-#Example - Retrieve-NestedValue -object '{"a":{"b":{"c":"d"}}}' -key 'a/b/c'
+#Example - Retrieve-NestedValue -object '{"a":{"b":{"c":"d"}}}' -keys @('a/b','a/b/c')
--- scripts/retrieve-resource-metadata.ps1
+++ scripts/retrieve-resource-metadata.ps1
@@ -3,11 +3,11 @@
 2. Copy this and 'nested-value-retrieval.psm1' to a scripts folder.
 3. Open a terminal (Powershell Core is required)
 4. Run Powershell Core (psm1.exe)
-5. Set the $key variable to the key of the metadata key you want to retrieve (If this is a nested value use /) - Example 'compute/securityProfile/secureBootEnabled"
+5. Set the $key variable to the key of the metadata key you want to retrieve (If this is a nested value use /) - Example 'compute/securityProfile/secureBootEnabled", use a comma if you wish to retrieve more than 1 value
 6. Run this script
 #>
 
-$key = "compute/securityProfile/secureBootEnabled"
+$keys = "compute/securityProfile/secureBootEnabled,securityProfile"
 import-module ./scripts/nested-value-retrieval.psm1
 $metadata = Invoke-RestMethod -Method GET -Uri "http://169.254.169.254/metadata/instance?api-version=2019-03-11" -Headers @{"Metadata"="True"}
-Retrieve-NestedValue -object $metadata -key $key
+Retrieve-NestedValue -object $metadata -keys $keys
